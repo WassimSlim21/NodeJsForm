1c359ce6d8779017bebd0f0fe280dbec3ce098ce
wassimslim21 <wassimslim18@gmail.com>
2022-02-12 19:24:45 +0100

    handling request push

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const express = require("express");
 //setting up express up 
 const app = express();
 
+app.get('/api', function(req, res){
+    console.log('GET Rquest');
+    res.end();
+})
+
+app.delete('/api', function(req, res){
+    console.log('GET Rquest');
+    res.send({name: "Yoshi"});
+})
 //listen for request 
 
 app.listen(process.env.port || 4000, function(){
